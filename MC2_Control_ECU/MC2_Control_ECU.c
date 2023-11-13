bee02f7decce773170f42cd9a259ce4ab81b3e1d
alinoureldeen43 <alinoureldeen43@gmail.com>
2023-11-13 16:01:22 +0200

    first commit

---
+++ MC2_Control_ECU/MC2_Control_ECU.c
@@ -0,0 +1,19 @@
+/*
+ * MC2_Control_ECU.c
+ *
+ *  Created on: May 8, 2023
+ *      Author: Ali Hassan
+ */
+
+
+#include "APP/app.h"
+
+int main()
+{
+	APP_init();
+
+	while(1)
+	{
+		APP_start();
+	}
+}
